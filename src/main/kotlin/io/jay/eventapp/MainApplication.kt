3f72a7f68a0288b2de6ac4e64d8ae87b8427b529
Jay Kim <tensaijskim1991@gmail.com>
2025-07-07 17:29:15 +0900

    setting up base

--- src/main/kotlin/io/jay/eventapp/MainApplication.kt
+++ src/main/kotlin/io/jay/eventapp/MainApplication.kt
@@ -1,19 +1,8 @@
 package io.jay.eventapp
 
-import io.jay.eventapp.domain.Story
-import io.jay.eventapp.dto.NewStoryRequest
-import io.jay.eventapp.service.StoryService
-import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.event.EventListener
-import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableAsync
-import org.springframework.stereotype.Component
-import org.springframework.transaction.event.TransactionPhase
-import org.springframework.transaction.event.TransactionalEventListener
 
 @EnableAsync
 @SpringBootApplication
@@ -22,60 +11,3 @@ class MainApplication
 fun main(args: Array<String>) {
     runApplication<MainApplication>(*args)
 }
-
-@Component
-class Runner(
-    private val storyService: StoryService,
-) : CommandLineRunner {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-    override fun run(vararg args: String?) {
-        storyService.create(NewStoryRequest(
-            title = "CI/CD pipeline",
-            description = "For acceptance environment"
-        ))
-
-        val stories = storyService.all()
-        logger.info("{}", stories.map { it.id })
-    }
-}
-
-@Component
-class MyEventListener {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-    @EventListener
-    fun handleApplicationReadyEvent(event: ApplicationReadyEvent) {
-        logger.info("Application started: {}", event)
-    }
-
-    /* Version 1 */
-//    @EventListener
-//    fun handleStoryCreatedEvent(event: StoryCreatedEvent) {
-//        logger.info("Story created: {}", event)
-//    }
-
-    /* Version 2 */
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    fun handleStoryCreatedEvent(event: StoryCreatedEvent) {
-        logger.info("Story created: {}", event)
-    }
-
-    @Async
-    @EventListener
-    fun handleBadEvent(event: ErrorEvent) {
-        logger.info("Story init: {}", event)
-        throw RuntimeException("error event")
-    }
-
-}
-
-data class StoryCreatedEvent(
-    val story: Story
-)
-
-data class ErrorEvent(
-    val story: Story
-)
--- src/main/kotlin/io/jay/eventapp/service/StoryService.kt
+++ src/main/kotlin/io/jay/eventapp/service/StoryService.kt
@@ -1,37 +1,13 @@
 package io.jay.eventapp.service
 
-import io.jay.eventapp.StoryCreatedEvent
-import io.jay.eventapp.ErrorEvent
 import io.jay.eventapp.domain.Story
-import io.jay.eventapp.dto.NewStoryRequest
 import io.jay.eventapp.repository.StoryRepository
-import org.slf4j.LoggerFactory
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StoryService(
     private val storyRepository: StoryRepository,
-    private val applicationEventPublisher: ApplicationEventPublisher
 ) {
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-    @Transactional
-    fun create(newStoryRequest: NewStoryRequest): Story {
-        logger.info("Creating a new story")
-        val saved = storyRepository.save(
-            Story(
-                id = 0L,
-                title = newStoryRequest.title,
-                description = newStoryRequest.description
-            )
-        )
-//        applicationEventPublisher.publishEvent(ErrorEvent(saved))
-        applicationEventPublisher.publishEvent(StoryCreatedEvent(saved))
-        logger.info("Returning saved story: {}", saved)
-        return saved
-    }
 
     fun all(): List<Story> {
         return storyRepository.findAll().toList()
